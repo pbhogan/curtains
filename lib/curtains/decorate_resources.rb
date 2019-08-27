1f40ffec1275c9dbf7f7643b5699df38bc9d969d
Patrick Hogan <pbhogan@gmail.com>
2019-08-27 11:29:03 -0500

    Removed alias_method_chain for Rails 5 compatibility.

--- lib/curtains/decorate_resources.rb
+++ lib/curtains/decorate_resources.rb
@@ -3,9 +3,17 @@ module Curtains
     extend ActiveSupport::Concern
 
     included do
-      alias_method_chain :resource, :decoration
-      alias_method_chain :build_resource, :decoration
-      alias_method_chain :collection, :decoration
+      # alias_method_chain :resource, :decoration
+      alias_method :resource_without_decoration, :resource
+      alias_method :resource, :resource_with_decoration
+
+      # alias_method_chain :build_resource, :decoration
+      alias_method :build_resource_without_decoration, :build_resource
+      alias_method :build_resource, :build_resource_with_decoration
+
+      # alias_method_chain :collection, :decoration
+      alias_method :collection_without_decoration, :collection
+      alias_method :collection, :collection_with_decoration
     end
 
 
--- lib/curtains/version.rb
+++ lib/curtains/version.rb
@@ -1,3 +1,4 @@
 module Curtains
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 end
+
